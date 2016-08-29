660474340d36ee5d01a99f03d24b943b47819579
Konstantin S. Uvarin <khedin@gmail.com>
2016-08-29 20:00:09 +0300

    DOC Initial commit - all

---
+++ lib/MVC/Neaf.pm
@@ -0,0 +1,111 @@
+package MVC::Neaf;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+MVC::Neaf - The great new MVC::Neaf!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use MVC::Neaf;
+
+    my $foo = MVC::Neaf->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Konstantin S. Uvarin, C<< <khedin at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mvc-neaf at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MVC-Neaf>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc MVC::Neaf
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MVC-Neaf>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MVC-Neaf>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MVC-Neaf>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/MVC-Neaf/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2016 Konstantin S. Uvarin.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of MVC::Neaf
