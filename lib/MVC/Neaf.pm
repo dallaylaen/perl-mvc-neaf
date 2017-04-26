130f0995db87ebe27b3793f9f2ba4ee8e7484b3b
Konstantin S. Uvarin <khedin@gmail.com>
2017-04-26 23:22:08 +0300

    API Add -head => [...] return value

--- lib/MVC/Neaf.pm
+++ lib/MVC/Neaf.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = 0.1604;
+our $VERSION = 0.1605;
 
 =head1 NAME
 
@@ -118,6 +118,9 @@ E.g. display generated image.
 It will be executed AFTER the headers and pre-generated content
 are served to the client, and may use C<$req-E<gt>write( $data );>
 and C<$req-E<gt>close;> to output more data.
+
+=item * -head - Pass a hash or array of values for header generation.
+This is an alternative to L<MVC::Neaf::Request>'s C<push_header> method.
 
 =item * -jsonp - Used by JS view module as a callback name to produce a
 L<jsonp|https://en.wikipedia.org/wiki/JSONP> response.
@@ -1483,8 +1486,19 @@ sub handle_request {
             unless $data->{-type} =~ /; charset=/;
     };
 
-    # Mangle headers - these modifications remain stored in req
+    # Mangle headers - NOTE these modifications remain stored in req
     my $head = $req->header_out;
+    if (my $append = $data->{-head}) {
+        if (ref $append eq 'ARRAY') {
+            for (my $i = 0; $i < @$append; $i+=2) {
+                $head->push_header($append->[$i], $append->[$i+1]);
+            };
+        }
+        else {
+            carp("Neaf: -head must be ARRAY, not ".(ref $append));
+            # Would love to die, but it's impossible here
+        };
+    };
     $head->init_header( content_type => $data->{-type} || $self->{-type} );
     $head->init_header( location => $data->{-location} )
         if $data->{-location};
