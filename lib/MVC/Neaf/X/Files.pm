00f846dd973d4db900375a4af87a44311e5e393e
Konstantin S. Uvarin <khedin@gmail.com>
2017-04-27 13:34:42 +0300

    MOD Add separate serve_file method to X::Files

--- lib/MVC/Neaf/X/Files.pm
+++ lib/MVC/Neaf/X/Files.pm
@@ -2,7 +2,7 @@ package MVC::Neaf::X::Files;
 
 use strict;
 use warnings;
-our $VERSION = 0.16;
+our $VERSION = 0.1601;
 
 =head1 NAME
 
@@ -10,9 +10,9 @@ MVC::Neaf::X::Files - serve static content for Not Even A Framework.
 
 =head1 SYNOPSIS
 
-     use MVC::Neaf;
+     use MVC::Neaf qw(:sugar);
 
-     MVC::Neaf->static( "/path/in/url" => "/local/path", %options );
+     neaf static "/path/in/url" => "/local/path", %options;
 
 These options would go to this module's new() method described below.
 
@@ -39,6 +39,8 @@ use parent qw(MVC::Neaf::X);
 =over
 
 =item * buffer - buffer size for serving files.
+Currently this is also the size below which in-memory caching is on,
+but this MAY change in the future.
 
 =item * cache_ttl - if given, files below the buffer size will be stored
 in memory for cache_ttl seconds.
@@ -61,9 +63,19 @@ sub new {
     return $class->SUPER::new(%options);
 };
 
-=head2 make_handler
+=head2 serve_file( $path )
 
-Returns a Neaf-compatible hander sub.
+Create a Neaf-compatible response using given path.
+The response is like follows:
+
+    {
+        -content => (file content),
+        -head => (length, name etc),
+        -type => (content-type),
+        -continue => (serve the rest of the file, if needed),
+    };
+
+Will C<die 404;> if file is not there.
 
 =cut
 
@@ -80,97 +92,113 @@ our %ExtType = (
     txt  => 'text/plain',
 );
 
-sub make_handler {
-    my $self = shift;
+sub serve_file {
+    my ($self, $file) = @_;
 
     my $bufsize = $self->{buffer};
     my $dir = $self->{root};
+    my $time = time;
+    my @header;
+
+    # sanitize file path before caching
+    $file = "/$file";
+    $file =~ s#/+#/#g;
+    $file =~ s#/$##;
+
+    if (my $data = $self->{cache_content}{$file}) {
+        if ($data->{expire} < $time) {
+            delete $self->{cache_content}{$file};
+        } else {
+            push @header, content_disposition => $data->{disposition}
+                if $data->{disposition};
+            $data->{expire_head} ||= http_date( $data->{expire} );
+            push @header, expires => $data->{expire_head};
+            return {
+                -content => $data->{data},
+                -type => $data->{type},
+                -head=>\@header,
+            };
+        };
+    };
+
+    # don't let unsafe paths through
+    $file =~ m#/../# and die 404;
+    $file =~ m#(^|/)\.# and die 404
+        unless $self->{allow_dots};
+
+    # open file
+    my $xfile = join "", $dir, $file;
+
+    die 404 if -d $xfile; # Sic! Don't reveal directory structure
+    my $ok = open (my $fd, "<", "$xfile");
+    if (!$ok) {
+        # TODO Warn
+        die 404;
+    };
+    binmode $fd;
+
+    my $size = [stat $fd]->[7];
+    local $/ = \$bufsize;
+    my $buf = <$fd>;
+
+    # determine type, fallback to extention
+    my $type;
+    $xfile =~ m#(?:^|/)([^\/]+?(?:\.(\w+))?)$#;
+    $type = $ExtType{lc $2} if defined $2;
+
+    my $show_name = $1;
+    $show_name =~ s/[\"\x00-\x19\\]/_/g;
+
+    my $disposition = ($type && $type =~ qr#^text|^image|javascript#)
+        ? ''
+        : "attachment; filename=\"$show_name\"";
+    push @header, content_disposition => $disposition
+            if $disposition;
+
+    # return whole file if possible
+    if ($size < $bufsize) {
+        if ($self->{cache_ttl}) {
+            my %content;
+            $content{data} = $buf;
+            $content{expire} = $time + $self->{cache_ttl};
+            $content{type} = $type;
+            $content{disposition} = $disposition;
+            $self->{cache_content}{$file} = \%content;
+        };
+        return { -content => $buf, -type => $type, -head => \@header }
+    };
+
+    # If file is big, print header & first data chunk ASAP
+    # then do the rest via a second callback
+    push @header, content_length => $size;
+    my $continue = sub {
+        my $req = shift;
+
+        local $/ = \$bufsize; # MUST do it again
+        while (<$fd>) {
+            $req->write($_);
+        };
+        $req->close;
+    };
+
+    return { -content => $buf, -type => $type, -continue => $continue, -head => \@header };
+};
+
+=head2 make_handler
+
+Returns a Neaf-compatible hander sub.
+
+=cut
+
+sub make_handler {
+    my $self = shift;
 
     # callback to be installed via stock ->route() mechanism
     my $handler = sub {
         my $req = shift;
 
         my $file = $req->path_info();
-        my $time = time;
-
-        # sanitize file path before caching
-        $file = "/$file";
-        $file =~ s#/+#/#g;
-        $file =~ s#/$##;
-
-        if (my $data = $self->{cache_content}{$file}) {
-            if ($data->{expire} < $time) {
-                delete $self->{cache_content}{$file};
-            } else {
-                $req->set_header( content_disposition => $data->{disposition} )
-                    if $data->{disposition};
-                $data->{expire_head} ||= http_date( $data->{expire} );
-                $req->set_header( expires => $data->{expire_head} );
-                return { -content => $data->{data}, -type => $data->{type} };
-            };
-        };
-
-        # don't let unsafe paths through
-        $file =~ m#/../# and die 404;
-        $file =~ m#(^|/)\.# and die 404
-            unless $self->{allow_dots};
-
-        # open file
-        my $xfile = join "", $dir, $file;
-
-        die 404 if -d $xfile; # Sic! Don't reveal directory structure
-        my $ok = open (my $fd, "<", "$xfile");
-        if (!$ok) {
-            # TODO Warn
-            die 404;
-        };
-        binmode $fd;
-
-        my $size = [stat $fd]->[7];
-        local $/ = \$bufsize;
-        my $buf = <$fd>;
-
-        # determine type, fallback to extention
-        my $type;
-        $xfile =~ m#(?:^|/)([^\/]+?(?:\.(\w+))?)$#;
-        $type = $ExtType{lc $2} if defined $2;
-
-        my $show_name = $1;
-        $show_name =~ s/[\"\x00-\x19\\]/_/g;
-
-        my $disposition = ($type && $type =~ qr#^text|^image|javascript#)
-            ? ''
-            : "attachment; filename=\"$show_name\"";
-        $req->set_header( content_disposition => $disposition )
-                if $disposition;
-
-        # return whole file if possible
-        if ($size < $bufsize) {
-            if ($self->{cache_ttl}) {
-                my %content;
-                $content{data} = $buf;
-                $content{expire} = $time + $self->{cache_ttl};
-                $content{type} = $type;
-                $content{disposition} = $disposition;
-                $self->{cache_content}{$file} = \%content;
-            };
-            return { -content => $buf, -type => $type }
-        };
-
-        # If file is big, print header & first data chunk ASAP
-        # then do the rest via a second callback
-        $req->set_header( content_length => $size );
-        my $continue = sub {
-            my $req = shift;
-
-            local $/ = \$bufsize; # MUST do it again
-            while (<$fd>) {
-                $req->write($_);
-            };
-            $req->close;
-        };
-
-        return { -content => $buf, -type => $type, -continue => $continue };
+        return $self->serve_file( $file );
     }; # end handler sub
 
     return $handler;
