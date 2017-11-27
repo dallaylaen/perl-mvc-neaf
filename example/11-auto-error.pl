6d962d56bb11bec5bef901ac9f10ad24964d9933
Konstantin S. Uvarin <khedin@gmail.com>
2017-11-27 10:34:03 +0200

    TST Add error handling demo, error doesn't work after all FIXME

---
+++ example/11-auto-error.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use MVC::Neaf qw(:sugar);
+
+my $tpl = <<"HTML";
+<html>
+<head>
+    <title>[% title | html %] - [% file | html %]</title>
+</head>
+<body>
+<h1>[% title | html %]</h1>
+<script lang="javascript">
+"use strict";
+var post_to = "/11/js";
+
+function upd() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != XMLHttpRequest.DONE)
+            return;
+        // pretend we forgot to check for http status
+        document.getElementById("content").innerHTML = xhr.responseText;
+    };
+    xhr.open( "get", post_to, true );
+    xhr.send();
+    return false;
+};
+</script>
+<div id="content">Not ready yet...</div>
+<input type="submit" value="Get data!" onClick="return upd()">
+
+<div>Don't forget to look at the server logs if you see anything unusual.</div>
+</body>
+</html>
+HTML
+
+get '/11/oops' => sub {
+    my $req = shift;
+    return {
+        file  => 'example/11 NEAF '.MVC::Neaf->VERSION,
+        title => 'Traceable error response',
+    };
+}, -template => \$tpl, -view => 'TT', description => "Unexpected error demo";
+
+get + post '/11/js' => sub {
+    die "Foobared";
+};
+
+neaf->run;
