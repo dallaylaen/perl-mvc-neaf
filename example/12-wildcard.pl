88d14fcc3aa099e92ac043b6816b9d36e5a6e2d1
Konstantin S. Uvarin <khedin@gmail.com>
2017-12-01 04:29:57 +0200

    TST Add comments to wildcard example MINOR

--- example/12-wildcard.pl
+++ example/12-wildcard.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/env perl
 
+# This example demonstrates forms with unforeknown fields.
+# We only know how the fields would look like, but not the exact list;
+#    collect ALL such fields, organize them in tuples, and
+#    show the form again with ability to add even more fields.
+
 use strict;
 use warnings;
 
@@ -28,37 +33,45 @@ my $tpl = <<'HTML';
                 <input name="stay[% item.id %]" value="[% item.stay | html %]">
             </div>
         [% END %]
-        Add guest [% max_guest %]: <i>name as word(s), arrival as time</i><br>
-        <input name="name[% max_guest %]">
-        <input name="stay[% max_guest %]">
+        Add guest [% last_index %]: <i>name as word(s), arrival as time</i><br>
+        <input name="name[% last_index %]">
+        <input name="stay[% last_index %]">
         <br>
         <input type="submit" value="Submit guests">
     </form>
 </body>
 HTML
 
-neaf form => guest => [ [ 'name\d+' => '\w+( +\w+)*' ], [ 'stay\d+' => '\d+:\d+' ] ],
-    engine => 'Wildcard';
+# Define form spec. This will be compiled once and applied to
+#    multiple requests.
+neaf form => guest => (
+    [ [ 'name\d+' => '\w+( +\w+)*' ], [ 'stay\d+' => '\d+:\d+' ] ],
+    engine => 'Wildcard',
+);
 
 get+post '/12/wildcard' => sub {
     my $req = shift;
 
+    # Get form object, all data inside
     my $guest = $req->form("guest");
 
+    # Clusterize the data
     my %tuples;
     foreach ($guest->fields) {
-        /^(\w+)(\d+)$/ or die "How this even possible?";
+        /^(\w+)(\d+)$/ or die "How is this even possible?";
         $tuples{$2}{$1} = $guest->raw->{$_};
         $tuples{$2}{error}++ if $guest->error->{$_};
         $tuples{$2}{id} = $2; # redundant, but simplifies processing
     };
 
+    # Listify the data
     my @list = sort { $a->{id} <=> $b->{id} }
         grep { $_->{name} } values %tuples;
 
+    # ... and return it for good
     return {
         guest_list => \@list,
-        max_guest  => @list ? ($list[-1]{id} + 1) : 1,
+        last_index => @list ? ($list[-1]{id} + 1) : 1,
         is_valid   => !grep { $_->{error} } @list,
     };
 }, default => {
